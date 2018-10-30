18f77fa6221a49b09554469d5e73ce704f01b68f
Blake Hoelle <bhoelle216@gmail.com>
2018-10-30 19:50:58 -0400

    Initial Commit

---
+++ DataDictonary-DynamicSQL/DataDictonary-DynamicSQL/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DataDictonary_DynamicSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbDataBaseNames = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbDataBaseNames
+            // 
+            this.cbDataBaseNames.FormattingEnabled = true;
+            this.cbDataBaseNames.Location = new System.Drawing.Point(12, 12);
+            this.cbDataBaseNames.Name = "cbDataBaseNames";
+            this.cbDataBaseNames.Size = new System.Drawing.Size(121, 21);
+            this.cbDataBaseNames.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbDataBaseNames);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbDataBaseNames;
+    }
+}
+
---
+++ DataDictonary-DynamicSQL/DataDictonary-DynamicSQL/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataDictonary_DynamicSQL
+{
+    public partial class Form1 : Form
+    {
+        const string connString = @"Server=PL11\MTCDEVDB;Database=master;Trusted_Connection=True;";
+        SqlConnection sqlConn = new SqlConnection(connString);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter sqlDa = new SqlDataAdapter("SelectAllDatabases", sqlConn);
+            DataTable dtDatabases = new DataTable();
+            int dbID;
+            string DatabaseName;
+            try
+            {
+                sqlDa.Fill(dtDatabases);
+
+                foreach(DataRow drDbName in dtDatabases.Rows)
+                {
+                    dbID = int.Parse(drDbName.ItemArray[1].ToString());
+                    DatabaseName = drDbName.ItemArray[0].ToString();
+                    cbDataBaseNames.Items.Add(new ComboObject(dbID, DatabaseName));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public class ComboObject
+        {
+            int dbID;
+            string dbName;
+
+            public ComboObject(int DatabaseID, string DatabaseName)
+            {
+                dbID = DatabaseID;
+                dbName = DatabaseName;
+            }
+
+            public string DatabaseName
+            {
+                get { return dbName; }
+                set { dbName = value; }
+            }
+
+            public int DatabaseID
+            {
+                get { return dbID; }
+                set { dbID = value; }
+            }
+        }
+    }
+}
